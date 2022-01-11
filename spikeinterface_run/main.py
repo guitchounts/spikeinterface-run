f7717bf875ebd55e4574c055b7328d9b896ca432
guitchounts <g.guitchounts@gmail.com>
2022-01-11 15:14:36 -0500

    adding option to skip pre-sorted sessions

--- spikeinterface_run/main.py
+++ spikeinterface_run/main.py
@@ -30,6 +30,7 @@ def cli():
 
 def shared_options(func):
     @click.argument('input-path', type=str, default='./') 
+    @click.option('--sorter-path', type=str, default='tmp_MS4', help="Folder name for sorting output. Default: tmp_MS4")
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         return func(*args, **kwargs)
@@ -38,13 +39,15 @@ def shared_options(func):
 @cli.command(name="launch")
 @click.option('--slurm/--no-slurm', default=False, help='Submit a slurm job for each .mkv file')
 @click.option('--cores', type=int, default=8, help="Number of cores")
-@click.option('--memory', type=str, default="64GB", help="RAM string")
+@click.option('--memory', type=str, default="32GB", help="RAM string")
 @click.option('--wall-time', type=str, default='4:00:00', help="Wall time")
 @click.option('--partition', type=str, default='short', help="Partition name")
 @click.option('--sorter', type=str, default='ms4', help="Sorter name. Defult = ms4")
-@click.option('--sorting_path', type=str, default=datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), help='Path to store slurm logs.')
+@click.option('--logs_path', type=str, default=datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), help='Path to store slurm logs.')
+@click.option('--skip-sorted', type=bool, default=True, help='Bool to skip already sorted files. Default: True')
+
 @shared_options
-def launch(input_path, sorting_path, slurm, cores, memory, wall_time, partition, sorter): #  # 
+def launch(input_path, logs_path, slurm, cores, memory, wall_time, partition, sorter, skip_sorted, sorter_path): #  # 
     
     print('Launching Sorting')
     
@@ -52,7 +55,10 @@ def launch(input_path, sorting_path, slurm, cores, memory, wall_time, partition,
     # get all open ephys files in the input_path:
     open_ephys_folders = glob('%s/**/Record*' % input_path,recursive=True)
     
-    
+    if skip_sorted:
+        # find sessions that already have the sorter_path in them and skip:
+        open_ephys_folders = [folder for folder in open_ephys_folders if not os.path.exists('%s/../%s' % (folder,sorter_path) ) ]
+
 
     if len(open_ephys_folders) > 0:
 
@@ -65,8 +71,8 @@ def launch(input_path, sorting_path, slurm, cores, memory, wall_time, partition,
             print(session)
             
 
-            os.system('sbatch -p {} -t {} --mem {} -c {} --wrap """spikeinterface-run submit {}""" '.format(
-                            partition, wall_time, memory, cores, session
+            os.system('sbatch -p {} -t {} --mem {} -c {} --wrap """spikeinterface-run submit {} --sorter-path {} """ '.format(
+                            partition, wall_time, memory, cores, session, sorter_path
                       ))
 
       
@@ -80,11 +86,11 @@ def launch(input_path, sorting_path, slurm, cores, memory, wall_time, partition,
 
 @cli.command(name="submit")
 @shared_options
-def submit(input_path):
+def submit(input_path,sorter_path):
     
     print('Submitting sorting on input path %s' % input_path)
 
-    run_sorting(input_path)    
+    run_sorting(input_path,sorter_path)    
 
 
 
